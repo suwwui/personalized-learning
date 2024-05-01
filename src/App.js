6e6813701452c956aaf1e7130dbb227100a49d5e
Fadhlina Wjedi <128072211+suwwui@users.noreply.github.com>
2024-05-01 12:35:26 +0800

    register-page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from "react";
-import Login from "./View/Login";
-
-
+import React from 'react'
+import Login from './View/Login'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Register from './View/Register'
 
 function App() {
+
   return (
-    <main>
-    <Login/>
-      </main>
-  );
+    <BrowserRouter>
+      <Routes>
+        < Route path='/' element={<Login />}></Route>
+        < Route path='/Register' element={<Register />}></Route>
+    </Routes>
+    </BrowserRouter >
+  )
 }
 
-export default App;
+export default App
--- src/View/Register.js
+++ src/View/Register.js
@@ -1,7 +1,5 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Checkbox from '@mui/material/Checkbox';
-import FormControlLabel from '@mui/material/FormControlLabel';
 import FormLabel from '@mui/material/FormLabel';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import Typography from '@mui/material/Typography';
@@ -9,7 +7,6 @@ import { Grid } from '@mui/material';
 import Button from '@mui/material/Button';
 import { Link } from "react-router-dom";
 import backgroundImage from "/Users/fadhlina/personalised-chatbot/src/Components/chatbot-background.jpg"
-
 import { styled } from '@mui/system';
 
 const FormGrid = styled('div')(() => ({
@@ -17,11 +14,10 @@ const FormGrid = styled('div')(() => ({
     flexDirection: 'column',
 }));
 
-export default function Login() {
+export default function Register() {
     const [userName, setUserName] = React.useState('');
-    const handleUsername = (event) => {
-
-    };
+    const [fullName, setfullName] = React.useState('');
+    const handleUsername = (event) => {};
 
     return (
         <Grid
@@ -44,8 +40,8 @@ export default function Login() {
                 xs={12} sm={2} md={4}
                 elevation={4}
                 sx={{
-                    height: '479px',
-                    width: '479px',
+                    height: '738px',
+                    width: '489px',
                     display: 'flex',
                     flexDirection: 'column',
                     justifyContent: 'center',
@@ -63,8 +59,8 @@ export default function Login() {
                         borderRadius: 1,
                     }}
                 >
-                    <Typography component="h1" variant="h5" padding='20px' margin='10px'>
-                        Welcome Back!
+                    <Typography component="h1" variant="h5"  margin='5px' mb='30px'>
+                       Please Fill Out Form to Register!
                     </Typography>
 
                     <Box
@@ -76,7 +72,23 @@ export default function Login() {
                         }}
                     >
                         <FormGrid sx={{ flexGrow: 1 }}>
-                            <FormLabel htmlFor="username" >
+                            <FormLabel >
+                                Full Name
+                            </FormLabel>
+                            <OutlinedInput
+                                id="fullName"
+                                autoComplete="fulllName"
+                                placeholder=""
+                                required
+                                value={fullName}
+                                onChange={handleUsername}
+                                sx={{ backgroundColor: 'white', width: '100%' }}
+                            />
+                        </FormGrid>
+                    </Box>
+                    <Box sx={{ display: 'flex', width: '100%', padding: '10px' }}>
+                        <FormGrid sx={{ flexGrow: 1 }}>
+                            <FormLabel  >
                                 Username
                             </FormLabel>
                             <OutlinedInput
@@ -84,16 +96,27 @@ export default function Login() {
                                 autoComplete="username"
                                 placeholder=""
                                 required
-                                value={userName}
-                                onChange={handleUsername}
                                 sx={{ backgroundColor: 'white', width: '100%' }}
                             />
                         </FormGrid>
-
                     </Box>
                     <Box sx={{ display: 'flex', width: '100%', padding: '10px' }}>
                         <FormGrid sx={{ flexGrow: 1 }}>
-                            <FormLabel htmlFor="password" >
+                            <FormLabel  >
+                               Email
+                            </FormLabel>
+                            <OutlinedInput
+                                id="Email"
+                                autoComplete="Email"
+                                placeholder=""
+                                required
+                                sx={{ backgroundColor: 'white', width: '100%' }}
+                            />
+                        </FormGrid>
+                    </Box>
+                    <Box sx={{ display: 'flex', width: '100%', padding: '10px' }}>
+                        <FormGrid sx={{ flexGrow: 1 }}>
+                            <FormLabel >
                                 Password
                             </FormLabel>
                             <OutlinedInput
@@ -104,13 +127,20 @@ export default function Login() {
                                 sx={{ backgroundColor: 'white', width: '100%' }}
                             />
                         </FormGrid>
-
                     </Box>
-                    <Box sx={{ display: 'flex', width: '100%', justifyContent: 'flex-start' }}>
-                        <FormControlLabel
-                            control={<Checkbox name="saveInfo" />}
-                            label="Remember me"
-                        />
+                    <Box sx={{ display: 'flex', width: '100%', padding: '10px' }}>
+                        <FormGrid sx={{ flexGrow: 1 }}>
+                            <FormLabel >
+                                Confirm Password
+                            </FormLabel>
+                            <OutlinedInput
+                                id="confirmPassword"
+                                autoComplete="confirmPassword"
+                                placeholder=""
+                                required
+                                sx={{ backgroundColor: 'white', width: '100%' }}
+                            />
+                        </FormGrid>
                     </Box>
                     <Button
                         type="submit"
@@ -127,17 +157,12 @@ export default function Login() {
                             }
                         }}
                     >
-                        Login
+                        Register
                     </Button>
-                    <Grid item xs>
-                        <Link href="#" variant="body2">
-                            Forgot password?
-                        </Link>
-                    </Grid>
                     <Grid item>
-                             <Typography>
-                            Don't have an account?
-                            <Link to="./Register">Register here</Link>
+                        <Typography>
+                            I have an account
+                            <Link to="/"> Login</Link>
                         </Typography>
                     </Grid>
 
@@ -146,4 +171,3 @@ export default function Login() {
         </Grid>
     );
 }
-
